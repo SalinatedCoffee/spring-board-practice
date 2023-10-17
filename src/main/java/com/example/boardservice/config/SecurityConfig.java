92d16ae0e570db84aa1712181244a8fe3a7f9c33
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-10-17 20:49:33 +0900

    Add and configure Spring Security

---
+++ src/main/java/com/example/boardservice/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.boardservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+        // .formLogin() marked for deprecation in 7
+        // instead use .formLogin(Customizer.withDefaults()) to use defaults
+        .formLogin(Customizer.withDefaults())
+        .build();
+  }
+}

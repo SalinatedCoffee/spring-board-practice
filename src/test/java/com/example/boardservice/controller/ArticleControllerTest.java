cf5e922bb0ef8471056214e9d47eae7f061d50de
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-10-17 20:50:37 +0900

    Configure tests to go through Spring Security and fix single article page test

--- src/test/java/com/example/boardservice/controller/ArticleControllerTest.java
+++ src/test/java/com/example/boardservice/controller/ArticleControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.boardservice.controller;
 
+import com.example.boardservice.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,6 +17,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("View controller: articles")
+// configure tests to go through spring security
+@Import(SecurityConfig.class)
 // only load relevant bean
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
@@ -47,7 +51,7 @@ class ArticleControllerTest {
         .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
         .andExpect(view().name("articles/detail"))
         .andExpect(model().attributeExists("article"))
-        .andExpect(view().name("articleComments"));
+        .andExpect(model().attributeExists("articleComments"));
   }
 
   @Disabled("Not yet implemented")

f2a7611a3aaac89b27f5db8306d398ba4f3a1a93
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-10-17 20:50:00 +0900

    Add authentication view tests

---
+++ src/test/java/com/example/boardservice/controller/AuthControllerTest.java
@@ -0,0 +1,34 @@
+package com.example.boardservice.controller;
+
+import com.example.boardservice.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View controller: authentication")
+@Import(SecurityConfig.class)
+@WebMvcTest
+class AuthControllerTest {
+  private final MockMvc mvc;
+
+  public AuthControllerTest(@Autowired MockMvc mvc) {
+    this.mvc = mvc;
+  }
+
+  @DisplayName("[View][GET] Login page (board) - normal call")
+  @Test
+  public void givenNothing_whenTryingToLogIn_thenReturnsLoginView() throws Exception {
+    // given
+    // when & then
+    mvc.perform(get("/login"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+  }
+}

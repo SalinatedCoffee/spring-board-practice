f9ea5fc9e509deb0b9ed0198397ffdc51d95db96
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-02-22 16:00:25 +0900

    Add comment

--- src/main/java/com/example/boardservice/domain/type/SearchType.java
+++ src/main/java/com/example/boardservice/domain/type/SearchType.java
@@ -2,6 +2,7 @@ package com.example.boardservice.domain.type;
 
 import lombok.Getter;
 
+// in Java, enums can have multiple params. Read: https://docs.oracle.com/javase/tutorial/java/javaOO/enum.html
 public enum SearchType {
   TITLE("Title"),
   CONTENT("Content"),

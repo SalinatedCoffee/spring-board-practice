98b00e966ab36f349d868a3fb8558ece38f1398b
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-02-26 20:40:57 +0900

    Add constructor that excludes DTO metadata

--- src/main/java/com/example/boardservice/dto/UserAccountDto.java
+++ src/main/java/com/example/boardservice/dto/UserAccountDto.java
@@ -1,6 +1,7 @@
 package com.example.boardservice.dto;
 
 import com.example.boardservice.domain.UserAccount;
+import org.apache.catalina.User;
 
 import java.time.LocalDateTime;
 
@@ -15,6 +16,10 @@ public record UserAccountDto(
     LocalDateTime modifiedAt,
     String modifiedBy
 ) {
+
+  public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo) {
+    return new UserAccountDto(userId, userPassword, email, nickname, memo, null, null, null, null);
+  }
   public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
     return new UserAccountDto(userId, userPassword, email, nickname, memo, createdAt, createdBy, modifiedAt, modifiedBy);
   }

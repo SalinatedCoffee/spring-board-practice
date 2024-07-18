c7527af2a0b600d15fd09880268041453ae6bc9e
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-07-18 17:27:06 +0900

    Fix view routing when logging in and out

--- src/main/java/com/example/boardservice/config/SecurityConfig.java
+++ src/main/java/com/example/boardservice/config/SecurityConfig.java
@@ -29,24 +29,24 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(MvcRequestMatcher.Builder mvc, HttpSecurity http) throws Exception {
     http
         .authorizeHttpRequests(auth -> auth
-                // don't enforce authorization on static resources (css, js, images... etc
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                .requestMatchers(mvc.pattern(HttpMethod.GET, "/")).permitAll()
-                .requestMatchers(mvc.pattern(HttpMethod.GET, "/articles")).permitAll()
-                .requestMatchers(mvc.pattern(HttpMethod.GET, "/articles/search-hashtag")).permitAll()
-                .anyRequest().authenticated())
-        // .formLogin() marked for deprecation in 7
-        // instead use .formLogin(Customizer.withDefaults()) to use defaults
+            // don't enforce authorization on static resources (css, js, images... etc
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+            .requestMatchers(mvc.pattern(HttpMethod.GET, "/")).permitAll()
+            .requestMatchers(mvc.pattern(HttpMethod.GET, "/articles")).permitAll()
+            .requestMatchers(mvc.pattern(HttpMethod.GET, "/articles/search-hashtag")).permitAll()
+            .anyRequest().authenticated())
+            // .formLogin() marked for deprecation in 7
+            // instead use .formLogin(Customizer.withDefaults()) to use defaults
         .formLogin(Customizer.withDefaults())
-            .logout(logout -> logout.logoutUrl("/login"));
+        .logout(logout -> logout.logoutSuccessUrl("/"));
     return http.build();
   }
 
-  @Bean
-  public WebSecurityCustomizer webSecurityCustomizer() {
-    // don't protect static resources(stylesheets, js, images... etc)
-    return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-  }
+//  @Bean
+//  public WebSecurityCustomizer webSecurityCustomizer() {
+//    // don't protect static resources(stylesheets, js, images... etc)
+//    return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+//  }
 
   @Bean
   public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {

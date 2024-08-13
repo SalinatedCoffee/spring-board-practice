73bdd9e4961380bc9a4c0a90871c9fd3ea044776
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-08-13 19:59:23 +0900

    Add support for parent-child relationship among articlecomment entities, as well as hierarchical ordering of comments linked to an article

--- src/main/java/com/example/boardservice/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/example/boardservice/dto/response/ArticleCommentResponse.java
@@ -1,9 +1,13 @@
 package com.example.boardservice.dto.response;
 
+import com.example.boardservice.domain.ArticleComment;
 import com.example.boardservice.dto.ArticleCommentDto;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 // the main reason for implementing separate response dtos is to decouple the original dtos from the controller layer
 // because we have implemented a special response dto for use in controllers, the controller layer can have zero
@@ -15,11 +19,22 @@ public record ArticleCommentResponse(
     LocalDateTime createdAt,
     String email,
     String nickname,
-    String userId
+    String userId,
+    Long parentCommentId,
+    Set<ArticleCommentResponse> childComments
 ) implements Serializable {
 
   public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
-    return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId);
+    return ArticleCommentResponse.of(id, content, createdAt, email, nickname, userId, null);
+  }
+
+  public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId) {
+    // custom comparator that compares comments first based on created datetime, and then their comment ids
+    Comparator<ArticleCommentResponse> childCommentComparator = Comparator
+        .comparing(ArticleCommentResponse::createdAt)
+        .thenComparingLong(ArticleCommentResponse::id);
+    // custom comparator above is used here to enforce ordering of elements in a set
+    return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator));
   }
 
   public static ArticleCommentResponse from(ArticleCommentDto dto) {
@@ -28,14 +43,18 @@ public record ArticleCommentResponse(
       nickname = dto.userAccountDto().userId();
     }
 
-    return new ArticleCommentResponse(
+    return ArticleCommentResponse.of(
         dto.id(),
         dto.content(),
         dto.createdAt(),
         dto.userAccountDto().email(),
         nickname,
-        dto.userAccountDto().userId()
+        dto.userAccountDto().userId(),
+        dto.parentCommentId()
     );
   }
 
+  public boolean hasParentComment() {
+    return parentCommentId != null;
+  }
 }
--- src/main/java/com/example/boardservice/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/example/boardservice/dto/response/ArticleWithCommentsResponse.java
@@ -1,12 +1,13 @@
 package com.example.boardservice.dto.response;
 
+import com.example.boardservice.dto.ArticleCommentDto;
 import com.example.boardservice.dto.ArticleWithCommentsDto;
 import com.example.boardservice.dto.HashtagDto;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Set;
-import java.util.LinkedHashSet;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentsResponse(
@@ -42,10 +43,36 @@ public record ArticleWithCommentsResponse(
         dto.userAccountDto().email(),
         nickname,
         dto.userAccountDto().userId(),
-        dto.articleCommentDtos().stream()
-            .map(ArticleCommentResponse::from)
-            .collect(Collectors.toCollection(LinkedHashSet::new))
+        organizeChildComments(dto.articleCommentDtos())
     );
   }
 
+  private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+    // convert set of articlecomment dtos into map of articlecommentresponses
+    // where the keys are the ids of each articlecomment
+    Map<Long, ArticleCommentResponse> map = dtos.stream()
+        .map(ArticleCommentResponse::from)
+        .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity()));
+
+    // then find all child comments and 'register' them to their parents
+    map.values().stream()
+//        .filter(ArticleCommentResponse::hasParentComment)
+        .filter(comment -> comment.hasParentComment())
+        .forEach(comment -> {
+          ArticleCommentResponse parentComment = map.get(comment.parentCommentId());
+          parentComment.childComments().add(comment);
+        });
+
+    // finally, filter out comments that are not children and order the remaining comments,
+    // sort them, then return an ordered set
+    return map.values().stream()
+        .filter(comment -> !comment.hasParentComment())
+        .collect(Collectors.toCollection(() ->
+            new TreeSet<>(Comparator
+                .comparing(ArticleCommentResponse::createdAt)
+                .reversed()
+                .thenComparingLong(ArticleCommentResponse::id))));
+    // essentially, the original set of all comments bound to the article are 'collapsed' into a 2-level
+    // structure
+  }
 }

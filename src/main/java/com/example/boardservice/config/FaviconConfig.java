1fb1ceebf5abc29e2c030698793ef7d0968f42d3
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-08-20 15:46:08 +0900

    Suppress Thymeleaf unwrapped fragment expression warnings and turn off default favicon

--- src/main/java/com/example/boardservice/config/FaviconConfig.java
+++ src/main/java/com/example/boardservice/config/FaviconConfig.java
@@ -1,12 +1,14 @@
 package com.example.boardservice.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Configuration
 public class FaviconConfig {
+  @Controller
   static class FaviconController {
     @RequestMapping(value = "favicon.ico", method = RequestMethod.GET)
     @ResponseBody

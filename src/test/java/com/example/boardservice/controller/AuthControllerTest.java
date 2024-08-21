ac2b97b9d8abf530a5894a424e5242efcd9ac9e5
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-08-21 21:33:54 +0900

    Implement unit tests for new modules, as well as update existing tests

--- src/test/java/com/example/boardservice/controller/AuthControllerTest.java
+++ src/test/java/com/example/boardservice/controller/AuthControllerTest.java
@@ -1,26 +1,35 @@
 package com.example.boardservice.controller;
 
-import com.example.boardservice.config.SecurityConfig;
 import com.example.boardservice.config.TestSecurityConfig;
+import com.example.boardservice.service.ArticleService;
+import com.example.boardservice.service.PaginationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("View controller: authentication")
 @Import(TestSecurityConfig.class)
-@WebMvcTest(Void.class)
+@WebMvcTest(AuthControllerTest.EmptyController.class)
 class AuthControllerTest {
 
   private final MockMvc mvc;
+
+  @MockBean
+  private PaginationService paginationService;
+  @MockBean
+  private ArticleService articleService;
 
   public AuthControllerTest(@Autowired MockMvc mvc) {
     this.mvc = mvc;
@@ -34,7 +43,12 @@ class AuthControllerTest {
     // when & then
     mvc.perform(get("/login"))
         .andExpect(status().isOk())
-        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-        .andDo(MockMvcResultHandlers.print());
+        .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    then(articleService).shouldHaveNoInteractions();
+    then(paginationService).shouldHaveNoInteractions();
   }
+
+  // optional, but use empty controller to make explicit the fact that this test suite does not require any controllers
+  @TestComponent
+  static class EmptyController {}
 }

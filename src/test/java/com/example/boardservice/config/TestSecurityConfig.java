bdb8859482f416d4c325c38c6fe43ced641f550d
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-08-21 21:33:01 +0900

    Update test suite security config to interact with UserAccountService instead of UserAccountRepository when mocking authenticated user

--- src/test/java/com/example/boardservice/config/TestSecurityConfig.java
+++ src/test/java/com/example/boardservice/config/TestSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.example.boardservice.config;
 
-import com.example.boardservice.domain.UserAccount;
-import com.example.boardservice.repository.UserAccountRepository;
+import com.example.boardservice.dto.UserAccountDto;
+import com.example.boardservice.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
@@ -13,17 +13,24 @@ import static org.mockito.BDDMockito.given;
 
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
-    @MockBean
-    private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod
-    public void securitySetup() {
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
-                "unoTest",
-                "testpw",
-                "uno-test@email.com",
-                "uno-test",
-                "test memo"
-                )));
+    public void securitySetUp() {
+        given(userAccountService.searchUser(anyString()))
+            .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+            .willReturn(createUserAccountDto());
+    }
+
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+            "unoTest",
+            "pw",
+            "uno-test@email.com",
+            "uno-test",
+            "test memo"
+        );
     }
 }
